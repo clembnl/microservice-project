1b297ab97b607bcb642bb9a4b5c1b03fe22d1b69
clembnl <cl.bonal@free.fr>
2024-10-01 17:44:55 +0200

    module 1

--- services/orders/src/index.ts
+++ services/orders/src/index.ts
@@ -4,30 +4,7 @@ import { errorHandler } from './middleware/errorHandler';
 import { logger } from './middleware/logger';
 import { OrderController } from './orderController';
 import { AppDataSource } from './data-source';
-// import { KafkaStreams }from 'kafka-streams';
 
-// const kafkaStreamsConfig = {
-//   noptions: {
-//     'metadata.broker.list': 'kafka:9092',
-//     'group.id': 'order-stream-group',
-//     'client.id': 'order-stream',
-//     'enable.auto.commit': false,
-//   },
-// };
-
-// const kafkaStreams = new KafkaStreams(kafkaStreamsConfig);
-// const orderStream = kafkaStreams.getKStream('order-created');
-
-// // Aggregate orders by status
-// orderStream
-//   .mapJSONConvenience() // Assuming you're working with JSON messages
-//   .countByKey('status', 'orderCount') // Group by status and count orders
-//   .map((keyValue) => JSON.stringify(keyValue))  // Serialize the key-value pairs
-//   .to('order-analytics', 1); // Output results to a new topic
-
-// orderStream.start(); // Start the stream processing
-
-// Express app setup
 const app = express();
 const orderController = new OrderController();
 
